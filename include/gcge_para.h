fb72dc825326e0f473fbf8bf3237b7ed1e882f86
pase2017 <pase2017@163.com>
2018-10-05 12:13:01 +0800

    changed the structure of para and workspace

--- include/gcge_para.h
+++ include/gcge_para.h
@@ -21,74 +21,45 @@
 
 #include "gcge_type.h"
 
-typedef struct GCGE_ORTH_PARA_ {
-    //正交化用到的参数
-    GCGE_DOUBLE orth_zero_tol; //确定正交化中出现０向量的阈值
-    GCGE_DOUBLE reorth_tol; //确定是否进行重正交化的阈值
-    GCGE_INT    max_reorth_time; //最大的重正交化次数
-    GCGE_INT    print_orth_zero; //print_orthzero确定是否打印正交化过程中出现0向量的信息,0:不打印，1:打印
+//GCGE_INPUT_PARA包含用户可以调节的算法相关的参数
+//ev表示特征对，eval表示特征值，evec表示特征向量
+typedef struct GCGE_EIGSOL_PARA_ {
 
-}GCGE_ORTH_PARA;
-
-typedef struct GCGE_PARA_ {
-    GCGE_INT    nev; //要求解的特征值个数，默认值为６
-    GCGE_INT    ev_max_it; //gcg最大迭代次数
-    GCGE_INT    block_size; //分批计算，每批计算的特征值个数
-
-    //基本参数
-    GCGE_INT    if_lobgcg; //使用gcg或lobgcg,0:gcg,1:lobgcg
+    GCGE_INT    nev;        //要求解的特征值个数，默认值为６
+    GCGE_INT    ev_max_iter;//gcg最大迭代次数
+    GCGE_INT    block_size; //分批计算，每批计算的特征值个数, 默认为nev
     GCGE_INT    given_init_evec; //用户是否给定初始特征向量,0:用户不给定,1:用户给定
 
-    //检查收敛性用到的参数
-    GCGE_DOUBLE ev_tol; //判定特征值已收敛的阈值
+    GCGE_DOUBLE ev_tol;    //判定特征值已收敛的阈值
     char*       conv_type; //判定收敛的方式,1:绝对残差, 2:相对残差
-    GCGE_INT    num_unlock; //nunlock表示未锁定的特征对个数(0<=nunlock<=nev)
-    //GCGE_INT    unconv_bs; //本次迭代中需要计算 P W 的个数
-
-    //正交化用到的参数
     char*       orth_type; //确定是用Ａ正交还是Ｂ正交,1:A_ORTH, 2:B_ORTH
-    GCGE_ORTH_PARA *orth_para;
+                           //orth_type不能放到ORTH_PARA当中，因为是eigsol中要用到的
 
-    GCGE_DOUBLE multi_tol;//default:0.2,判断特征值重数（或距离较近）的阈值
+    GCGE_DOUBLE eval_multi_tol; //default:0.2,判断特征值重数（或距离较近）的阈值
+    GCGE_DOUBLE eval_multi_max; //检查特征值重数时最大检测到nev的多少倍
 
-    //GCGE内置LinearSolver(CG)参数
-    GCGE_INT    if_use_cg; //是否使用内置CG
-    GCGE_INT    cg_max_it; //CG最大迭代次数
-    GCGE_DOUBLE cg_rate; //终止CG迭代残差下降比例
-
-    //GCGE迭代次数,因为统计的是[X,P,W]都参与计算的迭代次数，所以从-1开始
-    GCGE_INT    num_iter;//初始化为-2
-    GCGE_DOUBLE max_res; //最大残差
-    GCGE_DOUBLE max_ind; //最大残差的位置
-    GCGE_DOUBLE min_res; //最小残差
-    GCGE_DOUBLE min_ind; //最小残差的位置
-    GCGE_DOUBLE sum_res; //残差和
-    GCGE_DOUBLE *res; //残差
-
-
-    //判定是否打印的参数
-    GCGE_INT    print_eval; //是否每次GCGE迭代后打印特征值
-    GCGE_INT    print_cg_error; //是否打印每次CG迭代的残差
-    GCGE_INT    print_orthzero; //print_orthzero确定是否打印正交化过程中出现0向量的信息,0:不打印，1:打印
-    GCGE_INT    print_para; //是否打印GCGE_PARA的参数信息
+    GCGE_INT    print_eval;   //是否每次GCGE迭代后打印特征值
     GCGE_INT    print_result; //是否打印最终结果以及每次迭代的收敛个数等
-    GCGE_INT    print_matlab; //如果要用matlab画图，那么就不要打印converged这些
     GCGE_INT    print_part_time; //是否打印各部分的时间
-    GCGE_INT    print_level; //用打印层级来控制打印多少东西
-    //GCGE_STATISTIC_PARA *stat_para;
 
-}GCGE_PARA;
+}GCGE_EIGSOL_PARA;
 
-/**
- * @brief 
- *
- * @param para
- * @param nev
- */
-GCGE_INT GCGE_PARA_SetFromCommandLine(GCGE_PARA *para, GCGE_INT argc, char **argv);
-void GCGE_PARA_Create(GCGE_PARA **para);
-void GCGE_PARA_Free(GCGE_PARA **para);
-void GCGE_PARA_SetNumEigen(GCGE_PARA *para, GCGE_INT nev);
-void GCGE_PARA_Setup(GCGE_PARA *para);
+GCGE_INT GCGE_EIGSOL_PARA_SetFromCommandLine(GCGE_PARA *para, GCGE_INT argc, char **argv);
+//para不需要创建和销毁，只需要初始化
+void GCGE_EIGSOL_PARA_Initialize(GCGE_EIGSOL_PARA *para); //初始化参数
+void GCGE_EIGSOL_PARA_SetNumEigen(GCGE_EIGSOL_PARA *para, GCGE_INT nev);
+void GCGE_EIGSOL_PARA_SetBlockSize(GCGE_EIGSOL_PARA *para, GCGE_INT block_size);
+void GCGE_EIGSOL_PARA_SetEigenvalueTolerance(GCGE_EIGSOL_PARA *para, GCGE_DOUBLE ev_tol);
+void GCGE_EIGSOL_PARA_SetConvergenceType(GCGE_EIGSOL_PARA *para, GCGE_CHAR *conv_type);
+void GCGE_EIGSOL_PARA_SetOrthogonalizeType(GCGE_EIGSOL_PARA *para, GCGE_CHAR *orth_type);
+void GCGE_EIGSOL_PARA_SetEvalMultiplicityTolerance(GCGE_EIGSOL_PARA *para, GCGE_DOUBLE *eval_multi_tol);
+void GCGE_EIGSOL_PARA_SetEvalMultiplicityMaximumIndex(GCGE_EIGSOL_PARA *para, GCGE_DOUBLE *eval_multi_max);
+void GCGE_EIGSOL_PARA_SetPrintEval(GCGE_EIGSOL_PARA *para, GCGE_INT *print_eval);
+void GCGE_EIGSOL_PARA_SetPrintResult(GCGE_EIGSOL_PARA *para, GCGE_INT *print_result);
+void GCGE_EIGSOL_PARA_SetPrintPartTime(GCGE_EIGSOL_PARA *para, GCGE_INT *print_part_time);
+
+//为了防止参数互斥，比如我们设置nev的时候，就把可能会互斥的参数也设置一遍
+//对于nev会影响的参数block_size，设置的时候，如果互斥了，要给一个提示，block_size不能比nev大
+//要考虑到参数设置的先后关系，互相影响关系
 #endif
 
--- include/gcge_workspace.h
+++ include/gcge_workspace.h
@@ -23,44 +23,26 @@
 #include "gcge_para.h"
 #include "gcge_ops.h"
 
-typedef struct GCGE_WORKSPACE_ {
-    //向量工作空间
-    void       **V; //V用于存储[X,P,W]基底
-    void       **V_tmp; //V_tmp用作临时的向量存储空间
-    /* TODO */
-    void       **RitzVec; //RitzVec用于存储Ritz向量
+typedef struct GCGE_EIGSOL_WS_ {
 
-    void       **CG_p; //CG迭代中的p向量
-    void       **CG_r; //CG迭代中的r向量
+    void        **V; //V用于存储[X,P,W]基底
+    void        **V_tmp; //V_tmp用作临时的向量存储空间
+    void        **RitzVec; //RitzVec用于存储Ritz向量
+    GCGE_INT    space_size_V; //给V分配空间的大小，向量个数
+    GCGE_INT    space_size_V_tmp; //给V_tmp分配空间的大小，向量个数
+    GCGE_INT    space_size_RitzVec; //给RitzVec分配空间的大小，向量个数
 
     GCGE_INT    *unlock; //unlock用于存储未锁定的特征对编号(0-nev)
-    GCGE_INT    max_dim_x; //X中最多的向量个数
-    GCGE_INT    dim_x; //X中的向量个数
-    GCGE_INT    dim_xp; //[X,P]中的向量个数
-    GCGE_INT    dim_xpw; //[X,P,W]中的向量个数
-    GCGE_INT    last_dim_xpw; //上次GCGE迭代中[X,P,W]中的向量个数
-    GCGE_INT    last_dim_x; //上次GCGE迭代中X中的向量个数
-
-    GCGE_INT    conv_bs; //全部收敛的批次的特征值个数
-    GCGE_INT    unconv_bs; //本批次中未收敛的特征值个数
-
-    //子空间操作用到的工作空间
-    void        *dense_matrix; //稠密子空间矩阵
 
     GCGE_DOUBLE *subspace_matrix; //子空间矩阵
     GCGE_DOUBLE *subspace_evec; //子空间特征向量
-    GCGE_DOUBLE *subspace_dtmp; //小规模的工作空间，原来的AA_sub等都放到work_space
-    GCGE_INT    *subspace_itmp; 
+    GCGE_DOUBLE *subspace_eval; //子空间矩阵的特征值
+    GCGE_DOUBLE *subspace_dtmp; //小规模的double型工作空间
+    GCGE_INT    *subspace_itmp; //小规模的int型工作空间
 
-    GCGE_DOUBLE *eval; //子空间矩阵的特征值
-
-    //正交化用到的工作空间
-    GCGE_INT    *orth_ind; //用于正交化过程中存储非零向量的编号
-}GCGE_WORKSPACE;
-
-//一些结构体的空间的创建与销毁
-void GCGE_WORKSPACE_Create(GCGE_WORKSPACE **workspace);
-void GCGE_WORKSPACE_Setup(GCGE_WORKSPACE *workspace, GCGE_PARA *para, GCGE_OPS *ops, void *A);
-void GCGE_WORKSPACE_Free(GCGE_WORKSPACE **workspace, GCGE_PARA *para, GCGE_OPS *ops);
+}GCGE_EIGSOL_WS;
+//eigsol工作空间的创建与销毁
+void GCGE_EIGSOL_WS_Create(GCGE_EIGSOL_WS **eigsol_ws, GCGE_PARA *para, GCGE_OPS *ops, void *A);
+void GCGE_EIGSOL_WS_Free(GCGE_EIGSOL_WS **eigsol_ws, GCGE_OPS *ops);
 
 #endif
