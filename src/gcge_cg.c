b5847367c38c6b32e1250a458c4b2d55d31ed831
pase2017 <pase2017@163.com>
2018-10-05 17:43:33 +0800

    hhxie changed cg and orth, nzhang changed para and ws

--- src/gcge_cg.c
+++ src/gcge_cg.c
@@ -26,51 +26,66 @@
 void GCGE_CG(void *Matrix, void *b, void *x, GCGE_OPS *ops, GCGE_PARA *para, void **V_tmp)
 {
     //临时向量
-    void        *r, *p, *tmp;
+    void        *r, *p, *w;
     GCGE_INT    niter = 0;
     GCGE_INT    max_it = para->cg_max_it;
     GCGE_DOUBLE rate = para->cg_rate;
-    GCGE_DOUBLE tmp1, tmp2, alpha, beta, error, last_error;
+    GCGE_DOUBLE tmp1, tmp2, alpha, beta, rho1, rho2, error, last_error, pTw;
 
     //CG迭代中用到的临时向量
-    r   = V_tmp[1];
-    p   = V_tmp[2];
-    tmp = V_tmp[3];
-  
-    ops->MatDotVec(Matrix,x,p); //tmp1 = A*x0
- 
-    ops->VecAxpby(1.0, b, 0.0, r);
-    ops->VecAxpby(-1.0, p, 1.0, r);//r=b-p=r-p
-    ops->VecInnerProd(r, r, &error);//用残量的模来判断误差
-    error = sqrt(error);
-    ops->VecAxpby(1.0, r, 0.0, p);
-
-    do{
-        ops->MatDotVec(Matrix,p,tmp);//tmp = A*p
-        ops->VecInnerProd(r,p,&tmp1);
-        ops->VecInnerProd(p,tmp,&tmp2);    
-        alpha = tmp1/tmp2;
-        ops->VecAxpby(alpha, p, 1.0, x);   
-        ops->VecAxpby(-alpha, tmp, 1.0, r);
-        last_error = error;
-        ops->VecInnerProd(r, r, &error);   //用残量的模来判断误差
-        error = sqrt(error);
-    
-        if(error/last_error < rate)
+    r = V_tmp[1]; //记录残差向量
+    p = V_tmp[2];  //记录下降方向
+    w = V_tmp[3];  //记录tmp=A*p
+    ops->MatDotVec(Matrix,x,r); //tmp = A*x
+    ops->VecAxpby(1.0, b, -1.0, r);  
+    ops->VecInnerProd(r, r, &rho2);//用残量的模来判断误差
+    error = sqrt(rho2);
+    /* TODO */
+    //这里应该判断以下如果error充分小就直接返回!!!
+    //printf("the initial residual: %e\n",error);
+    //ops->VecAxpby(1.0, r, 0.0, p);
+    niter = 1;
+    last_error = error;
+    //max_it = 40;
+    //rate = 1e-5;
+    //printf("Rate = %e,  max_it = %d\n",rate,max_it);
+    while((last_error/error >= rate)&&(niter<max_it))
+    {
+        if(niter == 1)
         {
-            //printf("error: %e, last_error: %e, error/last_error: %e, rate: %e\n", 
-            //    error, last_error, error/last_error, rate);
-            break;
+            //set the direction as r
+            ops->VecAxpby(1.0, r, 0.0, p);
+            //p = r;
         }
-    
-        //beta = -(r,tmp)/(p,tmp)
-        ops->VecInnerProd(r,tmp,&tmp1);
-        ops->VecInnerProd(p,tmp,&tmp2);    
-        beta = -tmp1/tmp2;
-        ops->VecAxpby(1.0, r, beta, p);    
-
-        niter++; 
-    }while((error/last_error >= rate)&&(niter<max_it));
+        else
+        {
+            //printf("come here!\n");
+            //compute the value of beta
+            beta = rho2/rho1; 
+            //compute the new direction: p = r + beta * p
+            ops->VecAxpby(1.0, r, beta, p);
+        }//end for if(niter == 1)
+        //compute the vector w = A*p
+        ops->MatDotVec(Matrix,p,w);
+        //compute the value pTw = p^T * w 
+        ops->VecInnerProd(p, w, &pTw);
+        //compute the value of alpha
+        //printf("pTw=%e\n",pTw);
+        alpha = rho2/pTw; 
+        //compute the new solution x = alpha * p + x
+        ops->VecAxpby(alpha, p, 1.0, x);
+        //compute the new residual: r = - alpha*w + r
+        ops->VecAxpby(-alpha, w, 1.0, r);
+        //set rho1 as rho2
+        rho1 = rho2;
+        //compute the new rho2
+        ops->VecInnerProd(r, r, &rho2);  
+        last_error = sqrt(rho2);      
+        //printf("  niter= %d,  The current residual: %10.5f\n",niter, last_error);
+        //printf("  error=%10.5f, last_error=%10.5f,  last_error/error= %10.5f\n",error, last_error, last_error/error);
+        //update the iteration time
+        niter++;   
+    }
 
 }
 
---
+++ src/gcge_eigsol_ws.c
@@ -0,0 +1,115 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  gcge_eigsol_ws.c
+ *
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  2018年09月24日 09时57分13秒
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+#include "gcge_eigsol_ws.h"
+
+//给GCGE_EIGSOL_WS结构中各个部分分配空间
+void GCGE_EIGSOL_WS_Create(GCGE_EIGSOL_WS **eigsol_ws, GCGE_EIGSOL_PARA *para, 
+		GCGE_OPS *ops, void *A)
+{
+    (*eigsol_ws) = (GCGE_EIGSOL_WS *)malloc(sizeof(GCGE_EIGSOL_WS));
+
+	//RitzVec临时存储Ｘ, X的个数上限为nev加重数
+	GCGE_INT size_RitzVec = (GCGE_INT)(para->nev * (1.0+para->eval_multi_max));
+	//V存储[X,P,W],P,W的个数上限为block_size
+	GCGE_INT size_V       = size_RitzVec + 2 * para->block_size;
+	//V_tmp在正交化中(块:block_size,GS:1), RR中(VTAW:block_size),
+	//ComputeP中(暂时存储Ｐ:block_size),ComputeW(rhs:1),CG(3)
+	GCGE_INT size_V_tmp   = (para->block_size > 4)?(para->block_size):4;
+
+	//子空间矩阵存储VTAV,大小上限与Ｖ的空间上限有关
+	GCGE_INT size_submat  = size_V * size_V;
+	//subeval需要算X的上限个数
+	GCGE_INT size_subeval = size_RitzVec;
+	//subevec需要算Ｘ的上限个数,进行Ｐ的计算时需要再加Ｐ的个数列
+	GCGE_INT size_subevec = size_V * (size_V + para->block_size);
+	//dsyevr: liwork(10*N),isuppz:(2*N); dsyevx: liwork(5*N),ifail(N)
+	GCGE_INT size_subitmp = 12 * size_V;
+    //lwork: dsyevr(26*N,实际测试33*N),dsyevx(8*N,实际测试更多),dsyev(3*N),dsyevd(1+6*N+2*N**2)
+	//临时存储submat
+	GCGE_INT size_subdtmp = 33 * size_V + 3 * size_V * size_V;
+
+	(*eigsol_ws)->space_size_RitzVec = size_RitzVec;
+	(*eigsol_ws)->space_size_V       = size_V;
+	(*eigsol_ws)->space_size_V_tmp   = size_V_tmp;
+    
+	(*eigsol_ws)->unlock = (GCGE_INT*)calloc(para->nev, sizeof(GCGE_DOUBLE));
+	(*eigsol_ws)->subspace_matrix = (GCGE_DOUBLE*)calloc(size_submat,  sizeof(GCGE_DOUBLE));
+	(*eigsol_ws)->subspace_evec   = (GCGE_DOUBLE*)calloc(size_subevec, sizeof(GCGE_DOUBLE));
+	(*eigsol_ws)->subspace_eval   = (GCGE_DOUBLE*)calloc(size_subeval, sizeof(GCGE_DOUBLE));
+	(*eigsol_ws)->subspace_dtmp   = (GCGE_DOUBLE*)calloc(size_subdtmp, sizeof(GCGE_DOUBLE));
+	(*eigsol_ws)->subspace_itmp   = (GCGE_INT*)calloc(size_subitmp, sizeof(GCGE_DOUBLE));
+
+    ops->BuildMultiVecByMat(A, &((*eigsol_ws)->V),       size_V,       ops);
+    ops->BuildMultiVecByMat(A, &((*eigsol_ws)->V_tmp),   size_V_tmp,   ops);
+    ops->BuildMultiVecByMat(A, &((*eigsol_ws)->RitzVec), size_RitzVec, ops);
+}
+
+//释放GCGE_Para结构中分配的空间
+void GCGE_EIGSOL_WS_Free(GCGE_EIGSOL_WS **eigsol_ws, GCGE_OPS *ops)
+{
+    if((*eigsol_ws)->unlock)
+    {
+        free((*eigsol_ws)->unlock);
+        (*eigsol_ws)->unlock = NULL;
+    }
+    if((*eigsol_ws)->subspace_matrix)
+    {
+        free((*eigsol_ws)->subspace_matrix);
+        (*eigsol_ws)->subspace_matrix = NULL;
+    }
+    if((*eigsol_ws)->subspace_evec)
+    {
+        free((*eigsol_ws)->subspace_evec);
+        (*eigsol_ws)->subspace_evec = NULL;
+    }
+    if((*eigsol_ws)->subspace_eval)
+    {
+        free((*eigsol_ws)->subspace_eval);
+        (*eigsol_ws)->subspace_eval = NULL;
+    }
+    if((*eigsol_ws)->subspace_dtmp)
+    {
+        free((*eigsol_ws)->subspace_dtmp);
+        (*eigsol_ws)->subspace_dtmp = NULL;
+    }
+    if((*eigsol_ws)->subspace_itmp)
+    {
+        free((*eigsol_ws)->subspace_itmp);
+        (*eigsol_ws)->subspace_itmp = NULL;
+    }
+    if((*eigsol_ws)->V)
+    {
+        ops->FreeMultiVec(&((*eigsol_ws)->V), (*eigsol_ws)->space_size_V, ops);
+    }
+    if((*eigsol_ws)->V)
+    {
+        ops->FreeMultiVec(&((*eigsol_ws)->V_tmp), (*eigsol_ws)->space_size_V_tmp, ops);
+    }
+    if((*eigsol_ws)->V)
+    {
+        ops->FreeMultiVec(&((*eigsol_ws)->RitzVec), (*eigsol_ws)->space_size_RitzVec, ops);
+    }
+
+    free((*eigsol_ws)); (*eigsol_ws) = NULL;
+}
