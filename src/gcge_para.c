370d41a6d2806d87dc363fa00c46b7ca5d024452
pase2017 <pase2017@163.com>
2018-11-24 11:38:04 +0800

    modified gcge_para.c, the init value of w_orth_block_size

--- src/gcge_para.c
+++ src/gcge_para.c
@@ -360,7 +360,7 @@ void GCGE_PARA_Setup(GCGE_PARA *para)
     }
     if(para->orth_para->w_orth_block_size == 0)
     {
-        para->orth_para->w_orth_block_size = para->block_size/5;
+        para->orth_para->w_orth_block_size = (para->block_size/5 > 1)?(para->block_size/5) : 1;
     }
     para->orth_para->orth_zero_tol += DBL_EPSILON;
     para->orth_para->scbgs_reorth_tol += DBL_EPSILON;
